eb2efa9b333548da5f12c311ff6354c16508aff2
Restu Akbar <rebartheblues1905@gmail.com>
2024-08-07 21:19:17 +0700

    add: feature show detail product and create product

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+const Product = require('./models/product')
 
 // connect database
 mongoose.connect('mongodb://127.0.0.1/shopApp_db').then((result) => {
@@ -13,8 +11,23 @@ mongoose.connect('mongodb://127.0.0.1/shopApp_db').then((result) => {
     console.log(err)
 });
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', {products})    
+})
+app.get('/products/create', (req, res) => {
+    res.render('products/create')    
+})
+app.get('/products/:id', async (req, res) => {
+    const  {id} = req.params
+    const product = await Product.findById(id)
+    res.render('products/show', {product})    
 })
 
 app.listen(8080, () => {
