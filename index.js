626681684ce6fd39f65ed25ebbf500849c7301d9
Restu Akbar <rebartheblues1905@gmail.com>
2024-08-08 22:35:02 +0700

    add: feature edit, delete and filter data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const app = express()
 const Product = require('./models/product')
 
@@ -13,23 +14,51 @@ mongoose.connect('mongodb://127.0.0.1/shopApp_db').then((result) => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', {products})    
+    const { category } = req.query
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+})
+app.post('/products', async (req, res) => {
+    const products = new Product(req.body)
+    await products.save()
+    res.redirect(`/products/${products._id}`)
 })
 app.get('/products/create', (req, res) => {
-    res.render('products/create')    
+    res.render('products/create')
 })
 app.get('/products/:id', async (req, res) => {
-    const  {id} = req.params
+    const { id } = req.params
     const product = await Product.findById(id)
-    res.render('products/show', {product})    
+    res.render('products/show', { product })
+})
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit', { product })
+})
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/products/${product._id}`)
+})
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect("/products")
 })
 
 app.listen(8080, () => {
-    console.log('Shop app listening on port http://localhost:8080')
+    console.log('Shop app listening on port http://localhost:8080/products')
 })
